c321be43b8c3d4eb9d34f2dda3c36b6373425a1d
Kayla Smart <kaylacsmart@gmail.com>
2022-08-28 22:49:30 -0400

    Completed deck shuffling.

--- Program.cs
+++ Program.cs
@@ -42,9 +42,26 @@ namespace ArraysWithCards
             Console.WriteLine($"Our deck now has {endCount} cards.");
 
             //Shuffle deck
+            int numberOfCards = deck.Count;
+            var random = new Random();
 
+            for (int i = 0; i < 52; i++)
+            {
+                //Step 1: Randomly select a card.
+                var card = random.Next(i);
 
-            //Display top two cards
+                //Step 2: Swap random card
+                var temp = deck[i];
+                deck[i] = deck[card];
+                deck[card] = temp;
+            }
+
+            //Print new deck
+            Console.WriteLine("New deck order: ");
+            for (int i = 0; i < 52; i++)
+            {
+                Console.WriteLine(deck[i]);
+            }
 
 
 
@@ -52,12 +69,49 @@ namespace ArraysWithCards
             //deal two cards
 
 
-            //deal two cards to multiple players
+            //deal two cards to multiple players; max 7 players
+            Console.Write("How many players? ");
+            var numberOfPlayers = Console.ReadLine();
+            int players = int.Parse(numberOfPlayers);
 
+            if (players < 8)
+            {
+                var handOne = new List<string>();
+                var handTwo = new List<string>();
+                //additional players
+                int i = 0;
+
+                while (i < players * 2)
+                {
+
+                    handOne.Add(deck[i]);
+                    handTwo.Add(deck[i + 1]);
+                    //how to add player based on user input?
+                    i = i + players;
+                }
+
+                Console.WriteLine("Player one has the " + handOne[0] + " and the " + handOne[1]);
+                Console.WriteLine("Player two has the " + handTwo[0] + " and the " + handTwo[1]);
+            }
+            else
+            {
+                Console.WriteLine("Too many players! Game can only have up to 7 players.");
+            }
 
             /*Epic Mode*/
 
             //War
+            var warOne = new List<string>();
+            var warTwo = new List<string>();
+            int count = 0;
+
+            while (count < deck.Count)
+            {
+
+                warOne.Add(deck[count]);
+                warTwo.Add(deck[count + 1]);
+                count = count + 2;
+            }
 
 
 
